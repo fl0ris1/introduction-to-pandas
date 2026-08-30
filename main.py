acdfa0f03a711dc5ea902a48655de5471be0b61c
Floris <149056628+fl0ris1@users.noreply.github.com>
2026-08-30 10:11:19 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 #load data from the csv file
-data=pd.read_csv("titanic.csv")
+data=pd.read_csv("cw/titanic.csv")
 
 print(data.head())
 print(data.head(10))
@@ -16,8 +16,17 @@ print(p_classAndname.head())
 print(p_classAndname.shape)
 print(p_classAndname.dtypes)
 
-kbenzodagga
+df = pd.DataFrame(data)
 
-'''df = pd.DataFrame(data)
+#combine multiple conditions together
+class2and3=data[(data["Pclass"]==2)|(data["Pclass"]==3)]
 
-print(df)'''
+print(class2and3[["Name","Pclass"]].head(15))
+
+#females 1st class & males 3rd class survived
+
+females1stclass=data[(data["Pclass"]==1)&(data["Sex"]=="female")]
+print(females1stclass.head(20))
+
+males3rdclasssurvived=data[(data["Pclass"]==3)&(data["Sex"]=="male")&(data["Survived"]==1)]
+print(males3rdclasssurvived.head(20))
